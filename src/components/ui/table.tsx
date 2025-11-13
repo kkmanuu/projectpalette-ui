3c8edc396e3b3b394da1a7b1ed005b516dc18882
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-11-14 01:54:08 +0300

    feat(ui): add reusable UI components (Tabs, Toggle, Tooltip, Toast, Textarea) with forwardRef, variants, and styling

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -11,6 +11,7 @@ const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableE
 );
 Table.displayName = "Table";
 
+
 const TableHeader = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
   ({ className, ...props }, ref) => <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />,
 );
